d109bf4aad34fb4d4ff52655b99f925a1934afae
namtit2932k <1851061711@e.tlu.edu.vn>
2021-05-25 19:09:36 +0700

    vn + navbar

--- views/customer/create.php
+++ views/customer/create.php
@@ -4,13 +4,13 @@ include_once('./assets/layouts/header.php');
     <form action="http://localhost/cnpm/index.php?controller=customer&action=processCreate" method="post">
       <div class="container-fruid" align="center">
       <div class="form-group col-4" align="left">
-      <h1 class="" style="text-align:left"></br>Add customer</h1>
-          <label for="">Name</label>
+      <h1 class="" style="text-align:left"></br>Thêm khách hàng</h1>
+          <label for="">Tên</label>
           <input type="text"class="form-control" name="name" id="" aria-describedby="helpId" placeholder="">
-          <label for="">Phone</label>
+          <label for="">Số điện thoại</label>
           <input type="text"class="form-control" name="phone" id="" aria-describedby="helpId" placeholder="">
-          <button type="submit" style="margin-top:15px" class="btn btn-primary">Add</button>
-          <a name="" id="" class="btn btn-secondary" href="index.php?controller=customer&action=index" role="button" style="margin-top:15px">Back</a>
+          <button type="submit" style="margin-top:15px" class="btn btn-primary">Thêm</button>
+          <a name="" id="" class="btn btn-secondary" href="index.php?controller=customer&action=index" role="button" style="margin-top:15px">Trở lại</a>
         </div>
         </div>
       </form>
--- views/customer/index.php
+++ views/customer/index.php
@@ -5,18 +5,18 @@ include_once('./assets/layouts/header.php');
     <div class="container">
     <form class="form-inline">
         <div class="form-group">
-        <h1 class="" style="text-align:left"></br>Customers</h1>
-        <a name="" id="" class="btn btn-primary ml-3 mt-5" href="index.php?controller=customer&action=create" role="button">Add <i class="fas fa-plus"></i></a>
+        <h1 class="" style="text-align:left"></br>Khách hàng</h1>
+        <a name="" id="" class="btn btn-primary ml-3 mt-5" href="index.php?controller=customer&action=create" role="button">Thêm <i class="fas fa-plus"></i></a>
         </div>
     </form>
         <table class="table table-striped">
             <thead class="thead-dark">
                 <tr>
                     <th>ID</th>
-                    <th>Name</th>
-                    <th>Phone</th>
-                    <th>Total</th>
-                    <th>Action</th>
+                    <th>Tên</th>
+                    <th>Số điện thoại</th>
+                    <th>Tổng chi</th>
+                    <th>Hành động</th>
                 </tr>
             </thead>
             <tbody>
