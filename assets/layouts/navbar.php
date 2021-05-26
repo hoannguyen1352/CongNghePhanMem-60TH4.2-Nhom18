e15ba6fb62dcb9d2b2bc0b3f13c19ca34741f975
namtit2932k <75006498+namtit2932k@users.noreply.github.com>
2021-05-26 11:14:41 +0700

    navbar

--- assets/layouts/navbar.php
+++ assets/layouts/navbar.php
@@ -3,6 +3,7 @@ if (User::isLogin()) {
 ?>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="index.php">Bàn cafe</a>
+    <a class="nav-link text-light" href="index.php?controller=customer&action=index">Khách hàng</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -13,7 +14,7 @@ if (User::isLogin()) {
         if (User::isadmin()) {
         ?>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Quản lý
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -23,11 +24,7 @@ if (User::isLogin()) {
         <?php
         }
         ?>
-        <!-- thêm các button chức năng vào đây -->
         </div>
-        <li class="nav-item">
-          <a class="nav-link" href="index.php?controller=customer&action=index">Customers</a>
-        </li>
         <li class="nav-item col-2">
           <a href="index.php?controller=user&action=logout" class="nav-link active">Đăng xuất</a>
         </li>
