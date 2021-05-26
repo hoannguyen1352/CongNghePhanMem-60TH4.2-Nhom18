e80d80b144a3f0b6613f51105d28385f4c677946
namtit2932k <75006498+namtit2932k@users.noreply.github.com>
2021-05-26 09:49:07 +0700

    navbar

--- assets/layouts/navbar.php
+++ assets/layouts/navbar.php
@@ -1,7 +1,7 @@
 <?php
 if (User::isLogin()) {
 ?>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="index.php">Bàn cafe</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
