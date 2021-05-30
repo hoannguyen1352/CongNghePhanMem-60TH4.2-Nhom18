2d642f2ba2fc62d003e90f4fcb7a7c1a097478b6
NguyenAn1307 <67278593+NguyenAn1307@users.noreply.github.com>
2021-05-30 10:09:29 +0700

    Update tableController.php

--- controllers/tableController.php
+++ controllers/tableController.php
@@ -1,9 +1,5 @@
 <?php
 include_once('./models/Table.php');
-include_once('./models/Drink.php');
-include_once('./models/Bill.php');
-include_once('./models/Billdetail.php');
-include_once('./models/Customer.php');
 class tableController
 {
     public function index()
