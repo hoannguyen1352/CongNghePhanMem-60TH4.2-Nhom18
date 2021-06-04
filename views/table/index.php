2e5414eb36d388e55e7810a69b0c896e01cbd9b1
namtit2932k <1851061711@e.tlu.edu.vn>
2021-06-04 18:10:21 +0700

    xoa ban

--- views/table/index.php
+++ views/table/index.php
@@ -19,12 +19,14 @@ include_once "./assets/layouts/navbar.php";
         <?php
         foreach ($data as $table) {
           echo '<div class="col-md-2">';
-          if ($table['status'] == '0') { if(User::isadmin()){echo '&nbsp;';}?>
+          if ($table['status'] == '0') { if(User::isadmin()){
+            echo '<a onclick="return confirm(\'Bạn có muốn xóa bàn này không?\')" href="./index.php?controller=table&action=delete&id='.$table['id'].'" role="button"><i class="far fa-times-circle"></i></a>';
+            }?>
             <a href="./index.php?controller=book&action=show&id=<?php echo $table['id'] ?>" class="m-2 mb-4 pt-4 pb-4 pr-5 pl-5 btn btn-secondary text-white" style="width:146px;" id="<?php echo $table['id'] ?>" name="table" onclick="">Bàn <?php echo $table['id'] ?></a>
           <?php } else {
             if(User::isadmin())
           {
-          echo '<a onclick="return confirm(\'Bạn có muốn xóa bàn này không?\')" href="./index.php?controller=table&action=delete&id='.$table['id'].'" role="button"><i class="far fa-times-circle"></i></a>';
+            echo '&nbsp;';
           }?> <a href="./index.php?controller=book&action=show&id=<?php echo $table['id'] ?>" class="m-2 mb-4 pt-4 pb-4 pr-5 pl-5 btn btn-success text-white" style="width:146px;" id="<?php echo $table['id'] ?>" name="table">Bàn <?php echo $table['id'] ?></a>
       <?php }
           echo '</div>';
